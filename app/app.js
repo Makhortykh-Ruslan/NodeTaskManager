6ac0c52a309f1fd9420dbde17803797ac23a072d
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2024-06-29 20:02:28 +0300

    add config file

--- app/app.js
+++ app/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
 const { json } = require("express");
 const todoRouter = require("../routes/todoRouter");
 
+dotenv.config({ path: "./config.env" });
+
 const app = express();
+
+if (process.env.NODDE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.use(json());
 app.use("/api/v1/todo", todoRouter);
