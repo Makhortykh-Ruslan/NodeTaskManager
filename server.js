6c2e1d497eee3d96276a96f6627bd3356d74ecb1
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2024-06-29 22:43:27 +0300

    add connection with mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const app = require("./app/app");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Cpnections done"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
