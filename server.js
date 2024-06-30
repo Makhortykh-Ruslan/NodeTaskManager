1c2289e56772fa8cb0bceb77ece11722725871a6
Makhortykh-Ruslan <59147778+Makhortykh-Ruslan@users.noreply.github.com>
2024-06-30 16:45:14 +0300

    test

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSORD
-);
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
-  .then(() => console.log("DB connected"));
+// const DB = process.env.DATABASE.replace(
+//   "<PASSWORD>",
+//   process.env.DATABASE_PASSORD
+// );
+//
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log("DB connected"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
